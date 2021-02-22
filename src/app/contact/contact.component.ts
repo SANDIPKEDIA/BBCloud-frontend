fe8d36c23974e3d3af3455583d36b61c0669c598
Shashwat Gupta <shashwatg@umbrellainfocare.com>
2021-02-22 10:18:59 +0530

    code fixed

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,48 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl,FormGroup } from '@angular/forms';
-import { UsersService } from '../users.service';
-
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import { ToastController } from "@ionic/angular";
+import { UsersService } from "../users.service";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss'],
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent implements OnInit {
-  myReactiveForm:FormGroup
-  list :any;
-  constructor(private user:UsersService) {
-    this.user.getData().subscribe(data2=>{
-      console.log("Prototype result",data2)
-      // this.data=data
-    })
-   }
+  myReactiveForm: FormGroup;
+  list: any;
+  constructor(private user: UsersService,public toastController: ToastController) {
+   
+  }
+
+  getProtoype(){
+    this.user.getData().subscribe((result) => {
+      console.log("Prototype result", result);
+      this.list = result["response"];
+    });
+  }
 
   ngOnInit() {
-  this.myReactiveForm = new FormGroup({
-    'name': new FormControl(null),
-    'description': new FormControl(null),
-    'catagory': new FormControl(null),
-    'designs_count': new FormControl(null)
-  })
-}
-onSubmit(){
-  console.log(this.myReactiveForm);
-  
-  this.user.savePrototype(this.myReactiveForm.value).subscribe(data=>{
-  // const objectKeys = Object.keys;
-    console.log("Prototype result",data);
-    this.list=data;
-    
-    console.log("this data: ",this.list);
-    // console.log(datas);
+
+    this.getProtoype();
+
+    this.myReactiveForm = new FormGroup({
+      name: new FormControl(null),
+      description: new FormControl(null),
+      catagory: new FormControl(null),
+      designs_count: new FormControl(null),
+    });
   }
-    
-  )} 
 
-  
+  onSubmit() {
+    console.log(this.myReactiveForm);
+
+    this.user.savePrototype(this.myReactiveForm.value).subscribe((data) => {
+      this.myReactiveForm.reset();
+      this.presentToast('Prototype added')
+      this.getProtoype();
+    });
+  }
+
+  async presentToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000,
+      color:'success'
+    });
+    toast.present();
+  }
 }
-
-
-
-
